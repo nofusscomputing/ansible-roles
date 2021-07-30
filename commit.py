06730be6969291e1ff511ebc39b65720faf7e932
Jon Lockwood <jonathon.lockwood@networkedweb.com>
2021-07-30 14:13:16 +0930

    build(CI): Validate commit messages and MR title

---
+++ commit.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+
+import gitlab
+import os
+import sys
+import getopt
+#import gitlab.v4.objects
+
+#from commitizen import Check
+#from commitizen import commands, config
+
+
+get_first_commit = False
+get_mr_title = False
+
+try:
+   opts, args = getopt.getopt(sys.argv[1:],"hic:t:ti:p:b",["commit","token=", "title", "project=", "branch="])
+
+except getopt.GetoptError:
+   print('test.py [-c | --commit] [-t | --token {token}]')
+   sys.exit(2)
+   
+for opt, arg in opts:
+    if opt == '-h':
+        print('[commit.py] -i <inputfile> -o <outputfile>')
+        sys.exit()
+    elif opt in ("-c", "--commit"):
+       #inputfile = arg
+        get_first_commit = True
+    elif opt in ("-t", "--token"):
+       ci_job_token = arg
+    elif opt in ("-ti", "--title"):
+       get_mr_title = True
+    elif opt in ("-p", "--project"):
+       project_id = arg
+    elif opt in ("-b", "--branch"):
+       git_branch = arg
+
+
+
+#try:
+#    if os.environ['CI_JOB_TOKEN']:
+#        ci_job_token = os.environ['CI_JOB_TOKEN']
+#except:
+#    pass
+#else:
+
+# private token or personal token authentication
+gl = gitlab.Gitlab('https://gitlab.com', private_token=ci_job_token)
+
+#project = gl.projects.get('28204898', lazy=True)
+project = gl.projects.get(project_id, lazy=True)
+
+project_mrs = project.mergerequests.list()
+#mrs = gl.mergerequests.list()
+
+for mr in project_mrs:
+    if mr.source_branch == git_branch and str(mr.source_project_id) == str(project_id) and str(mr.state) == 'opened':
+        mr_title = mr.title
+        mr_first_commit = mr.sha
+
+        #print('\n\nMR=[-{0}-]'.format(mr))
+
+
+if get_first_commit:
+
+    print('{0}'.format(mr_first_commit))
+    #for mr in gl.mergerequests.list():
+    #for mr in project.mergerequests.list():
+        #print('\n\nMR=[-{0}-]'.format(mr))
+        #print('\n\nsource_branch=[-{0}-]'.format(mr.source_branch))
+        #print('\n\nsha=[-{0}-]'.format(mr.sha))
+        #print('{0}'.format(mr.sha))
+
+if get_mr_title:
+
+    print('{0}'.format(mr_title))
