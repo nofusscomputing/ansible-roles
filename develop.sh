dd6ad7ea3cc636d1e995f64077902c918be5fa29
Jon Lockwood <jonathon.lockwood@networkedweb.com>
2021-08-04 13:29:36 +0930

    build(gitlab-ci): Use gitlab-ci job gitlab_release
    !7

--- develop.sh
+++ develop.sh
@@ -12,12 +12,12 @@ echo "[DEBUG] Upgrade Python PIP"
 pip install --upgrade pip
 
 echo "[DEBUG] install python requirements for ansible"
-pip install -r ./CI/ansible/requirements.txt
+pip install -r ./gitlab-ci/ansible/requirements.txt
 
 echo "[DEBUG] install python requirements for commitizen"
-pip install -r ./CI/commitizen/requirements.txt
+pip install -r ./gitlab-ci/conventional_commits/requirements.txt
 
 echo "[DEBUG] install python module cz_nfc"
-pip install CI/gitlab_release/python-module/cz_nfc/.
+pip install gitlab-ci/gitlab_release/python-module/cz_nfc/.
 
 echo "Development environment setup. when you have completed development you can clear the python virtual environment by running the command 'deactivate'"
